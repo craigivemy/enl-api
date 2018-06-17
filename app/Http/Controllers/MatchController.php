371e4009129eb6e37296e37a3c0717f1f57f88ff
Craig Ivemy <craigivemy@gmail.com>
2018-06-17 16:32:07 +0100

    Team, player, umpire, match models, routes, controllers

---
+++ app/Http/Controllers/MatchController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Match;
+use Illuminate\Http\Request;
+
+class MatchController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Match  $match
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Match $match)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Match  $match
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Match $match)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Match  $match
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Match $match)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/UmpireController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Umpire;
+use Illuminate\Http\Request;
+
+class UmpireController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Umpire  $umpire
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Umpire $umpire)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Umpire  $umpire
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Umpire $umpire)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Umpire  $umpire
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Umpire $umpire)
+    {
+        //
+    }
+}
