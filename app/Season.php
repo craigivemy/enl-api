4bd3edd50f66d4674133294c305a451540b18f4e
Craig Ivemy <craigivemy@gmail.com>
2018-08-07 18:02:11 +0100

    Comments

--- app/Season.php
+++ app/Season.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Season extends Model
 {
 
+
+    // this is quesitonable - should be hasMany? Not according to docs
     public function divisions()
     {
         return $this->belongsToMany('App\Division');
