e823c9ec70201d6bf49f905e61a306b10ad183dd
Craig Ivemy <craigivemy@gmail.com>
2018-08-08 09:58:13 +0100

    Confirm  'belongsToMany' method

--- app/Season.php
+++ app/Season.php
@@ -7,8 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Season extends Model
 {
 
-
-    // this is quesitonable - should be hasMany? Not according to docs
     public function divisions()
     {
         return $this->belongsToMany('App\Division');
