85a36d1a87acb1a3d4c0a9b9e6667f3920024e57
Craig Ivemy <craigivemy@gmail.com>
2018-08-10 17:45:52 +0100

    CLub factory and seeder

--- database/factories/ClubFactory.php
+++ database/factories/ClubFactory.php
@@ -2,8 +2,13 @@
 
 use Faker\Generator as Faker;
 
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(App\Club::class, function (Faker $faker) {
     return [
-        //
+        'name'              => $faker->company,
+        'primary_color'     => $faker->rgbColor,
+        'secondary_color'   => $faker->rgbColor,
+        'tertiary_color'    => $faker->rgbColor,
+        'logo_url'          => $faker->imageUrl(),
+        'narrative'         => $faker->sentences()
     ];
 });
