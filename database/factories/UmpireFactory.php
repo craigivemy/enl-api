bb873d6299c791f2cc9880dc61ca6b01c8454693
Craig Ivemy <craigivemy@gmail.com>
2018-09-09 11:24:08 +0100

    umpire factory

--- database/factories/UmpireFactory.php
+++ database/factories/UmpireFactory.php
@@ -1,9 +1,13 @@
 <?php
 
 use Faker\Generator as Faker;
+use Illuminate\Support\Facades\DB;
 
 $factory->define(Model::class, function (Faker $faker) {
+    $user_ids = DB::table('teams')->select('id')->get();
     return [
-        //
+        'forename'  => $faker->firstName,
+        'surname'   => $faker->lastName,
+        'team_id'   => $faker->randomElement($user_ids)
     ];
 });
