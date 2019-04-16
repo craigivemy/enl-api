53a5bb81b9b168fd699051b9b5f938e4abdfbccd
Craig Ivemy <craigivemy@gmail.com>
2019-04-16 20:45:03 +0100

    Modify Season Resource to return id also

--- app/Http/Resources/Season.php
+++ app/Http/Resources/Season.php
@@ -15,6 +15,7 @@ class Season extends JsonResource
     public function toArray($request)
     {
         return [
+            'id'            => $this->id,
             'name'          => $this->name,
             'start_date'    => $this->start_date,
             'end_date'      => $this->end_date,
