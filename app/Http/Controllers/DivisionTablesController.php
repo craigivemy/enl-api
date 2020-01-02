fce7afd39f0462fc11179d04c4f090a644032c0d
Craig Ivemy <craigivemy@gmail.com>
2020-01-02 17:16:32 +0000

    Working, option for all divisions in season

--- app/Http/Controllers/DivisionTablesController.php
+++ app/Http/Controllers/DivisionTablesController.php
@@ -25,9 +25,9 @@ class DivisionTablesController extends Controller
         $bonus_value = $settings->get('bonus_point_value')->setting_value;
 
 
-
-        $teams = DB::select(DB::raw(
-            "SELECT 
+        if ($division_id) {
+            $teams = DB::select(DB::raw(
+                "SELECT 
                     t.name AS team_name,
                     t.id as team_id,
                     SUM(win) AS win,
@@ -73,8 +73,57 @@ class DivisionTablesController extends Controller
                     
                     GROUP BY t.id
                     ORDER BY SUM(points) DESC, goal_difference DESC"
-        ));
+            ));
+        } else {
+            $teams = DB::select(DB::raw(
+                "SELECT 
+                    t.name AS team_name,
+                    t.id as team_id,
+                    SUM(win) AS win,
+                    SUM(draw) AS draw,
+                    SUM(loss) AS loss,
+                    SUM(goals_for) AS goals_for,
+                    SUM(goals_against) AS goals_against,
 
+                    SUM(goal_difference) as goal_difference,
+                    SUM(games_played) AS games_played,
+                    SUM(points) as points
+                    FROM teams t INNER JOIN
+                        division_season_team dst
+                        ON t.id = dst.team_id
+                    LEFT JOIN (
+                        SELECT home_id
+                            team_name,
+                            IF(home_SCORE > away_score, 1,0) win,
+                            IF(home_score = away_score, 1,0) draw,
+                            IF(home_score < away_score, 1,0) loss,
+                            home_score goals_for,
+                            away_score goals_against,
+                            home_score - away_score goal_difference,
+                            1 games_played,
+                            CASE WHEN home_score > away_score THEN '" . $win_value . "' WHEN home_score = away_score THEN '" . $draw_value . "' ELSE '" . $loss_value . "' END points
+                        FROM matches mat WHERE played = 1 AND season_id = '" . $season_id . "'
+                        
+                        UNION ALL 
+                        SELECT away_id,
+                            
+                            IF(home_score < away_score, 1, 0),
+                            IF(home_score = away_score, 1,0),
+                            IF(home_score > away_score, 1,0),
+                            away_score,
+                            home_score,
+                            away_score - home_score goal_difference,
+                            1 games_played,
+                            CASE WHEN home_score < away_score THEN '" . $win_value . "' WHEN home_score = away_score THEN '" . $draw_value . "' ELSE '" . $loss_value . "' END
+                        FROM matches
+                        WHERE played = 1 AND season_id = '" . $season_id . "'
+                        
+                        ) AS total ON total.team_name=t.id WHERE dst.season_id = '" . $season_id . "'
+                    
+                    GROUP BY t.id
+                    ORDER BY dst.division_id, SUM(points) DESC, goal_difference DESC"
+            ));
+        }
         return $teams;
 
 // todo - check OK to use variables here as from other DB fields
