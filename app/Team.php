842e95db463c3fdaa8a6384142f4a463c684786e
Craig Ivemy <craigivemy@gmail.com>
2020-10-15 21:23:34 +0100

    Algolia / laravel scout

--- app/Team.php
+++ app/Team.php
@@ -4,10 +4,12 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Laravel\Scout\Searchable;
 
 class Team extends Model
 {
     use softDeletes;
+    use Searchable;
     
     protected $guarded = [];
     protected $hidden = ['pivot'];
